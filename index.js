b1428c545160e7237902c07221631904b5585296
farhat sattar <farhatsattar8675@gmail.com>
2024-04-06 02:41:06 +0500

    Assignment2

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+//1. Hello World:
+console.log("Hello world");
+//2. Current Date:
+console.log("Today Date : 6-April-2024");
+//3. Random Number:
+console.log("10,20,35,48,55,68,72,89,99,100");
+//4. Your Name
+console.log("Hello Anaya");
+//5. Favorite Quote:
+console.log('"The future belongs to those who believe in the beauty of their dreams."');
